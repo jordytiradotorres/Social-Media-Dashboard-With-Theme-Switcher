b4349b6a8e06ae9eb8218458760d499f202617d6
Jordy Tirado <jordytiradotorres20@gmail.com>
2020-10-22 09:20:37 -0500

    initial files

---
+++ prepro/webpack.config.js
@@ -0,0 +1,73 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+
+const webpack = require("webpack");
+
+module.exports = {
+	entry: path.resolve(__dirname, "src/index.js"),
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "app.js",
+	},
+	devtool: "inline-source-map",
+	devServer: {
+		hot: true,
+		open: true,
+		port: 9000,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.pug$/,
+				use: ["pug-loader"],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: "babel-loader",
+			},
+			{
+				test: /\.css$/,
+				use: [
+					// el loder: miniCSSE... --> nos va a server para produccion
+					// para desarrollo es mas rapido inyectar en el navegador
+					// {
+					// 	loader: MiniCSSExtractPlugin.loader,
+					// },
+					"style-loader",
+					"css-loader",
+				],
+			},
+			{
+				test: /\.s[ac]ss$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.jpg|png|gif|webp|woff|woff2|svg|mp4|mp3|webm$/,
+				use: [
+					{
+						loader: "url-loader",
+						options: {
+							// si las imagenes son pequeñas usar este limit
+							limit: 90000,
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		// new webpack.HotModuleReplacementPlugin(),
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: "Social media",
+			template: path.resolve(__dirname, "src/pug/index.pug"),
+		}),
+		// esto va hacer para produccion
+		// new MiniCSSExtractPlugin({
+		// 	filename: "css/[name].css",
+		// }),
+	],
+};
